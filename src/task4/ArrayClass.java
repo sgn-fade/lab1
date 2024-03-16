ecb8480ad9c83afc59223748df1aa2064d6cea05
fade <dimabreusgo@gmail.com>
2024-03-16 17:00:31 +0200

    task4 complete, no matrix

--- src/task4/ArrayClass.java
+++ src/task4/ArrayClass.java
@@ -1,17 +1,30 @@
 package task4;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public abstract class ArrayClass {
-    public static Object createArray(Class<?> classObject, int[] args) {
+    public static Object createArray(Class<?> classObject, int args) {
         return Array.newInstance(classObject, args);
     }
-    public static void changeSize(Object[] array, int new_size) {
-        Object[] new_array = (Object[]) Array.newInstance(array.getClass(), new_size);
-        java.lang.System.arraycopy(array, 0, new_array,0,  array.length);
+    public static Object changeSize(Object array, int new_size) {
+        int length = Math.min(Array.getLength(array), new_size);
+        Object new_array =  Array.newInstance(array.getClass().getComponentType(), new_size);
+        java.lang.System.arraycopy(array, 0, new_array,0,  length);
+        return new_array;
     }
 
-    public static String getArrayString(Object array){
-        return array.toString();
+    public static String getArrayString(Object[] array){
+        return Arrays.deepToString(array);
+    }
+    public static Object[] getArray(Object arr){
+        if (arr instanceof Object[])
+            return (Object[])arr;
+        int arrlength = Array.getLength(arr);
+        Object[] outputArray = new Object[arrlength];
+        for(int i = 0; i < arrlength; ++i){
+            outputArray[i] = Array.get(arr, i);
+        }
+        return outputArray;
     }
 }
--- src/task4/Main.java
+++ src/task4/Main.java
@@ -3,8 +3,12 @@ package task4;
 
 public class Main {
     public static void main(String[] args){
-        Object array = ArrayClass.createArray(int.class, new int[]{5, 2});
-        //Object array = ArrayClass.createArray(java.lang.Integer.class, 5);
+        Object[] array = ArrayClass.getArray(ArrayClass.createArray(int.class, 5));
+        System.out.println(ArrayClass.getArrayString(new Object[]{array}));
+
+        array = (Object[]) ArrayClass.changeSize(array, 7);
+
         System.out.println(ArrayClass.getArrayString(array));
+
     }
 }
